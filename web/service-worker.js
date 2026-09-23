fecc498a84f2ff45fec9c5f5aaae75dc0df2249b
Sai Khurana <saikhurana@Sais-MacBook-Pro-2.local>
2026-09-23 23:51:17 +0530

    Version PWA assets for reliable updates

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "glyph-soundbox-v10";
-const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
+const CACHE = "glyph-soundbox-v11";
+const ASSETS = ["./", "index.html", "styles.css?v=11", "app.js?v=11", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
